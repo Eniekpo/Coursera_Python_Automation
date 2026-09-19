1e52fbccabc3d8c647d34f9f77de0c55f91d98e2
Eniekpo <johneniekpo4life@gmail.com>
2026-09-19 10:54:14 +0100

    area

--- Modules/Areas.py
+++ Modules/Areas.py
@@ -11,3 +11,12 @@ def rectangle(base, height):
 
 def circle(radius):
     return math.pi * (radius**2)  #[cite: 20]
+
+def square(side):
+    return side * side  #[cite: 20]
+
+def trapezoid(base1, base2, height):
+    return (base1 + base2) * height / 2  #[cite: 20]
+
+def donut(outer_radius, inner_radius):
+    return math.pi * (outer_radius**2 - inner_radius**2)  #[cite: 20]   
--- Modules/calculate.py
+++ Modules/calculate.py
@@ -1,5 +1,8 @@
 import Areas
 
-print("The area of a triangle with base 3 and height 4 is: " + str(Areas.triangle(9, 4)))
+print("The area of a triangle with base 9 and height 4 is: " + str(Areas.triangle(9, 4)))
 print("The area of a rectangle with base 3 and height 4 is: " + str(Areas.rectangle(8, 5))) 
-print("The area of a circle with radius 3 is: " + str(Areas.circle(7))) 
+print("The area of a circle with radius 7 is: " + str(Areas.circle(7))) 
+print("The area of a square with side 5 is: " + str(Areas.square(5)))
+print("The area of a trapezoid with base1 3, base2 4, and height 5 is: " + str(Areas.trapezoid(3, 4, 5)))
+print("The area of a donut with outer radius 5 and inner radius 3 is: " + str(Areas.donut(5, 3)))
