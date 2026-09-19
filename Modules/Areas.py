5bea9ff6de2d389b3f4b17f02b271f9c598ebf5a
Eniekpo <johneniekpo4life@gmail.com>
2026-09-19 10:39:19 +0100

    modules

---
+++ Modules/Areas.py
@@ -0,0 +1,13 @@
+import math
+
+
+def triangle(base, height):
+    return base * height / 2  #[cite: 20]
+
+
+def rectangle(base, height):
+    return base * height  #[cite: 20]
+
+
+def circle(radius):
+    return math.pi * (radius**2)  #[cite: 20]
---
+++ Modules/calculate.py
@@ -0,0 +1,5 @@
+import Areas
+
+print("The area of a triangle with base 3 and height 4 is: " + str(Areas.triangle(9, 4)))
+print("The area of a rectangle with base 3 and height 4 is: " + str(Areas.rectangle(8, 5))) 
+print("The area of a circle with radius 3 is: " + str(Areas.circle(7))) 
